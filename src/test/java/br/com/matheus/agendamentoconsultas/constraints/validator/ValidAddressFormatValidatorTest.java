5d3a71983fba237b00b2c178568077ec0cdf84d0
Matheus Paulino Ribeiro <mathpaulinor@gmail.com>
2022-11-21 00:08:14 -0300

    Testando a classe ValidAddressFormatValidator

---
+++ src/test/java/br/com/matheus/agendamentoconsultas/constraints/validator/ValidAddressFormatValidatorTest.java
@@ -0,0 +1,47 @@
+package br.com.matheus.agendamentoconsultas.constraints.validator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ValidAddressFormatValidatorTest {
+	@Autowired
+	private ValidAddressFormatValidator validAddressFormatValidator;
+	
+	@Test
+	void deveRetornarTrueCasoForPassadoUmEnderecoComOFormatoValido() {
+		String enderecoValido = "R. Amadeu Garcia, 33, Jardim Rosário, Guapiraba, MG, 37806-875";
+		Boolean result = validAddressFormatValidator.isValid(enderecoValido, null);
+		assertTrue(result);
+	}
+	
+	@Test
+	void deveEsperarUmaExceptionCasoNaoForPassadoNenhumEndereco() {
+		String enderecoNulo = null;
+		try {
+			validAddressFormatValidator.isValid(enderecoNulo, null);
+		} catch(Exception e) {
+			assertTrue(true);
+		}		
+	}
+	
+	@Test
+	void deveRetornarFalseCasoForPassadoUmEnderecoVazio() {
+		String enderecoVazio = "";
+		Boolean result = validAddressFormatValidator.isValid(enderecoVazio, null);
+		assertFalse(result);
+	}
+	
+	@Test
+	void deveRetornarFalseCasoForPassadoUmEnderecoMalEscrito() {
+		String enderecoMalEscrito = "ra Amadeu, um número, Jardim Rosário, Guarapira, MRSA, 3434343434343";
+		Boolean result = validAddressFormatValidator.isValid(enderecoMalEscrito, null);
+		assertFalse(result);
+	}
+}
