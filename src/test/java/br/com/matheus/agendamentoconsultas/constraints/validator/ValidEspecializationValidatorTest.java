2b828a20333a768b770cd0d1214fcc314d973fc1
Matheus Paulino Ribeiro <mathpaulinor@gmail.com>
2022-11-22 20:44:52 -0300

    Testando a classe ValidEspecializationValidator

---
+++ src/test/java/br/com/matheus/agendamentoconsultas/constraints/validator/ValidEspecializationValidatorTest.java
@@ -0,0 +1,38 @@
+package br.com.matheus.agendamentoconsultas.constraints.validator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ValidEspecializationValidatorTest {
+	
+	@Autowired
+	private ValidEspecializationValidator validEspecializationValidator;
+	
+	@Test
+	void deveRetornarVerdadeiroAoPassarUmaEspecializacaoExistenteNoSistemaComAGrafiaCorreta() {
+		String especializacaoNome = "Pediatria";
+		Boolean result = validEspecializationValidator.isValid(especializacaoNome, null);
+		assertTrue(result);
+	}
+	
+	@Test
+	void deveRetornarFalseAoPassarUmaEspecializacaoExistenteNoSistemaComAGrafiaIncorreta() {
+		String especializacaoNomeErrado = "Pedaitraia";
+		Boolean result = validEspecializationValidator.isValid(especializacaoNomeErrado, null);
+		assertFalse(result);
+	}
+	
+	@Test
+	void deveRetornarFalseAoPassarUmaEspecializacaoNaoExistenteNoSistema() {
+		String especializacaoNaoExistente = "Veterinaria";
+		Boolean result = validEspecializationValidator.isValid(especializacaoNaoExistente, null);
+		assertFalse(result);
+	}
+}
