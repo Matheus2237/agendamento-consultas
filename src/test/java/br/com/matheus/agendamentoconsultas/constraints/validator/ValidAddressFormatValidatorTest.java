7f6b214fad863c4131f941657978aded9e68af62
Matheus Paulino Ribeiro <mathpaulinor@gmail.com>
2022-11-21 21:50:31 -0300

    Adicionando um espaço entre as linhas

--- src/test/java/br/com/matheus/agendamentoconsultas/constraints/validator/ValidAddressFormatValidatorTest.java
+++ src/test/java/br/com/matheus/agendamentoconsultas/constraints/validator/ValidAddressFormatValidatorTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class ValidAddressFormatValidatorTest {
+	
 	@Autowired
 	private ValidAddressFormatValidator validAddressFormatValidator;
 	
