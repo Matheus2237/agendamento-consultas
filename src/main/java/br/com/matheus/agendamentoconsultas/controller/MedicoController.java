744b0382ac5f397fa1cef9031c37a738dafbaaec
Matheus2237 <mathpaulinor@gmail.com>
2024-04-26 01:47:32 -0300

    refactor: Implementação de regras de negócio em classes Services

--- src/main/java/br/com/matheus/agendamentoconsultas/controller/MedicoController.java
+++ src/main/java/br/com/matheus/agendamentoconsultas/controller/MedicoController.java
@@ -1,84 +1,66 @@
 package br.com.matheus.agendamentoconsultas.controller;
 
-import java.net.URI;
-import java.util.Optional;
-
+import br.com.matheus.agendamentoconsultas.controller.dto.RequestAtualizacaoMedicoDTO;
+import br.com.matheus.agendamentoconsultas.controller.dto.RequestCadastroMedicoDTO;
+import br.com.matheus.agendamentoconsultas.controller.dto.ResponseMedicoDto;
+import br.com.matheus.agendamentoconsultas.controller.dto.ResponseTodosMedicosDto;
+import br.com.matheus.agendamentoconsultas.model.Medico;
+import br.com.matheus.agendamentoconsultas.service.MedicoService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import br.com.matheus.agendamentoconsultas.controller.dto.MedicoDto;
-import br.com.matheus.agendamentoconsultas.controller.dto.VisualizarTodosMedicoDto;
-import br.com.matheus.agendamentoconsultas.controller.form.AtualizacaoMedicoForm;
-import br.com.matheus.agendamentoconsultas.controller.form.MedicoForm;
-import br.com.matheus.agendamentoconsultas.model.Medico;
-import br.com.matheus.agendamentoconsultas.repository.MedicoRepository;
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/medico")
 public class MedicoController {
 
-	@Autowired
-	private MedicoRepository medicoRepository;
-	
-	@GetMapping
-	public ResponseEntity<Page<VisualizarTodosMedicoDto>> visualizarTodos(@PageableDefault(page = 0, size = 20, sort = "nome", direction = Direction.ASC) Pageable pageable) {
-		Page<Medico> medicos = this.medicoRepository.findAll(pageable);
-		return ResponseEntity.ok().body(medicos.map(VisualizarTodosMedicoDto::new));
-	}
-	
-	@PostMapping
-	@Transactional
-	public ResponseEntity<MedicoDto> cadastrar(@RequestBody @Valid MedicoForm medicoForm, UriComponentsBuilder uriComponentsBuilder) {
-		Medico medico = medicoForm.toMedico();
-		this.medicoRepository.save(medico);
-		URI uri = uriComponentsBuilder.path("medico/{id}").buildAndExpand(medico.getId()).toUri();
-		return ResponseEntity.created(uri).body(new MedicoDto(medico));
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Object> detalharMedico(@PathVariable Long id) {
-		Optional<Medico> medicoOptional = this.medicoRepository.findById(id);
-		if (medicoOptional.isPresent()) {
-			return ResponseEntity.ok(new MedicoDto(medicoOptional.get()));
-		}
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Médico não encontrado.");
-	}
-	
-	@PutMapping("/{id}")
-	@Transactional
-	public ResponseEntity<Object> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoMedicoForm atualizacaoMedicoForm) {
-		Optional<Medico> medicoOptional = this.medicoRepository.findById(id);
-		if (medicoOptional.isPresent()) {
-			Medico medico = atualizacaoMedicoForm.atualizar(id, medicoRepository);
-			return ResponseEntity.ok(new MedicoDto(medico));
-		}
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Médico não encontrado.");
-	}
-	
-	@DeleteMapping("/{id}")
-	@Transactional
-	public ResponseEntity<Object> deletar(@PathVariable Long id) {
-		Optional<Medico> medicoOptional = this.medicoRepository.findById(id);
-		if (medicoOptional.isPresent()) {
-			this.medicoRepository.deleteById(id);
-			return ResponseEntity.ok().body("Médico excluído com sucesso.");
-		}
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Médico não encontrado.");
-	}
+    private final MedicoService medicoService;
+
+    @Autowired
+    public MedicoController(MedicoService medicoService) {
+        this.medicoService = medicoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ResponseTodosMedicosDto>> visualizarTodos(@PageableDefault(size = 20, sort = "nome", direction = Direction.ASC) Pageable pageable) {
+        Page<ResponseTodosMedicosDto> medicos = this.medicoService.visualizarTodos(pageable);
+        return ResponseEntity.ok().body(medicos);
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<ResponseMedicoDto> cadastrar(@RequestBody @Valid RequestCadastroMedicoDTO requestCadastroMedicoDTO, UriComponentsBuilder uriComponentsBuilder) {
+        Medico medico = this.medicoService.cadastrar(requestCadastroMedicoDTO);
+        URI uri = uriComponentsBuilder.path("medico/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new ResponseMedicoDto(medico));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseMedicoDto> detalharMedico(@PathVariable Long id) {
+        ResponseMedicoDto detalhesMedico = this.medicoService.detalharMedico(id);
+        return ResponseEntity.ok(detalhesMedico);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<ResponseMedicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid RequestAtualizacaoMedicoDTO requestAtualizacaoMedicoDTO) {
+        ResponseMedicoDto medicoAtualizado = this.medicoService.atualizar(id, requestAtualizacaoMedicoDTO);
+        return ResponseEntity.ok(medicoAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<String> deletar(@PathVariable Long id) {
+        this.medicoService.deletar(id);
+        return ResponseEntity.ok().body("Médico excluído com sucesso.");
+    }
 }
---
+++ src/main/java/br/com/matheus/agendamentoconsultas/service/PacienteService.java
@@ -0,0 +1,55 @@
+package br.com.matheus.agendamentoconsultas.service;
+
+import br.com.matheus.agendamentoconsultas.controller.dto.RequestAtualizacaoPacienteDTO;
+import br.com.matheus.agendamentoconsultas.controller.dto.RequestCadastroPacienteDTO;
+import br.com.matheus.agendamentoconsultas.controller.dto.ResponsePacienteDto;
+import br.com.matheus.agendamentoconsultas.controller.dto.ResponseTodosPacientesDto;
+import br.com.matheus.agendamentoconsultas.exception.PacienteNaoEncontradoException;
+import br.com.matheus.agendamentoconsultas.model.Paciente;
+import br.com.matheus.agendamentoconsultas.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import static java.util.Optional.ofNullable;
+
+public class PacienteService {
+
+    private final PacienteRepository pacienteRepository;
+
+    @Autowired
+    public PacienteService(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    public Page<ResponseTodosPacientesDto> visualizarTodos(Pageable pageable) {
+        Page<Paciente> pacientes = this.pacienteRepository.findAll(pageable);
+        return pacientes.map(ResponseTodosPacientesDto::new);
+    }
+
+    public Paciente cadastrar(RequestCadastroPacienteDTO requestCadastroPacienteDTO) {
+        Paciente paciente = requestCadastroPacienteDTO.toPaciente();
+        return this.pacienteRepository.save(paciente);
+    }
+
+    public ResponsePacienteDto detalharPaciente(Long id) {
+        Paciente paciente = this.pacienteRepository.findById(id)
+                .orElseThrow(PacienteNaoEncontradoException::new);
+        return new ResponsePacienteDto(paciente);
+    }
+
+    public ResponsePacienteDto atualizar(Long id, RequestAtualizacaoPacienteDTO atualizacao) {
+        Paciente paciente = this.pacienteRepository.findById(id)
+                .orElseThrow(PacienteNaoEncontradoException::new);
+        ofNullable(atualizacao.getNome()).ifPresent(paciente::setNome);
+        ofNullable(atualizacao.getTelefone()).ifPresent(paciente::setTelefone);
+        ofNullable(atualizacao.getEndereco()).ifPresent(paciente::setEndereco);
+        return new ResponsePacienteDto(paciente);
+    }
+
+    public void deletar(Long id) {
+        Long idPaciente = this.pacienteRepository.findById(id)
+                .orElseThrow(PacienteNaoEncontradoException::new).getId();
+        this.pacienteRepository.deleteById(idPaciente);
+    }
+}
