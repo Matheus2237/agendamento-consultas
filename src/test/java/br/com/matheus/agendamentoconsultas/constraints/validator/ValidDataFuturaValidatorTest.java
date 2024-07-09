2a234c8de0eb8a5ec81d07bb76432bd12806d633
Matheus2237 <mathpaulinor@gmail.com>
2024-07-09 20:20:54 -0300

    test: Teste do validator de data para consulta

---
+++ src/test/java/br/com/matheus/agendamentoconsultas/constraints/validator/ValidDataFuturaValidatorTest.java
@@ -0,0 +1,69 @@
+package br.com.matheus.agendamentoconsultas.constraints.validator;
+
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ValidDataFuturaValidatorTest {
+
+    @InjectMocks
+    private ValidDataFuturaValidator validator;
+
+    @Mock
+    private ConstraintValidatorContext contextMock;
+
+    private AutoCloseable mocks;
+
+    @BeforeEach
+    void setUp() {
+        mocks = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    @SneakyThrows
+    void tearDown() {
+        mocks.close();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provisionaDatasFuturas")
+    void deveRetornarTrueSeADataForFutura(String dataFutura) {
+        assertTrue(validator.isValid(dataFutura, contextMock),
+                "Deve passar para a data ".concat(dataFutura));
+    }
+
+    static Stream<String> provisionaDatasFuturas() {
+        return Stream.of(
+                LocalDate.now().plusDays(1).toString(),
+                LocalDate.now().plusDays(2).toString()
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provisionaDatasPassadas")
+    void deveRetornarFalseSeADataForAtualOuPassada(String dataInvalida) {
+        assertFalse(validator.isValid(dataInvalida, contextMock),
+                "Deve falhar para a data ".concat(dataInvalida));
+    }
+
+    static Stream<String> provisionaDatasPassadas() {
+        return Stream.of(
+                LocalDate.now().minusDays(2).toString(),
+                LocalDate.now().minusDays(1).toString(),
+                LocalDate.now().toString()
+        );
+    }
+}
