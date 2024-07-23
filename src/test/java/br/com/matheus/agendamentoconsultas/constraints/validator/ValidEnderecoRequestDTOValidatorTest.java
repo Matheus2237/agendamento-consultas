64a55496f0067b19ff6392359e24ed89f1ff434b
Matheus2237 <mathpaulinor@gmail.com>
2024-07-22 22:20:55 -0300

    test: Aumento da cobertura de testes usando Junit Pioneer

--- src/test/java/br/com/matheus/agendamentoconsultas/constraints/validator/ValidEnderecoRequestDTOValidatorTest.java
+++ src/test/java/br/com/matheus/agendamentoconsultas/constraints/validator/ValidEnderecoRequestDTOValidatorTest.java
@@ -6,6 +6,10 @@ import jakarta.validation.ConstraintValidatorContext;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junitpioneer.jupiter.cartesian.ArgumentSets;
+import org.junitpioneer.jupiter.cartesian.CartesianTest;
+import org.junitpioneer.jupiter.cartesian.CartesianTest.MethodFactory;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -30,42 +34,39 @@ class ValidEnderecoRequestDTOValidatorTest extends MockedUnitTest {
         assertTrue(validator.isValid(endereco, contextMock), "Endereco válido.");
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'XX', '1234567'",
-            "'Rua Fictícia', '100, bloco B, apartamento 202', 'Centro', 'Rio de Janeiro', 'YY', '8765432'",
-            "'Av. Brasil', '1500, sala 10', 'Comercial', 'Curitiba', 'ZZ', '1122334'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'XX', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'S1', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', '12', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'S!', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'SP', '1234567'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'SP', '123456789'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'SP', '12345-678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'SP', '12345A78'"
-    })
-    void deveRetornarFalseQuandoUmEnderecoTiverUFOuCEPInconsistentes(String logradouro, String numero, String bairro, String cidade, String uf, String cep) {
+    @CartesianTest
+    @MethodFactory("provisionaCEPOuUFInconsistentes")
+    void deveRetornarFalseQuandoUmEnderecoTiverUFOuCEPInconsistentes(String uf, String cep) {
+        String logradouro = "Av. Bandeirantes";
+        String numero = "21, andar 4, apartamento 42";
+        String bairro = "Jardim Planalto";
+        String cidade = "São Paulo";
         EnderecoRequestDTO endereco = new EnderecoRequestDTO(logradouro, numero, bairro, cidade, uf, cep);
         assertFalse(validator.isValid(endereco, contextMock));
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {
-            "null, '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'SP', '12345678'",
-            "'Av. Bandeirantes', null, 'Jardim Planalto', 'São Paulo', 'SP', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', null, 'São Paulo', 'SP', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', null, 'SP', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', null, '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'SP', null",
-            "'', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'SP', '12345678'",
-            "'Av. Bandeirantes', '', 'Jardim Planalto', 'São Paulo', 'SP', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', '', 'São Paulo', 'SP', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', '', 'SP', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', '', '12345678'",
-            "'Av. Bandeirantes', '21, andar 4, apartamento 42', 'Jardim Planalto', 'São Paulo', 'SP', ''"
-    }, nullValues = "null")
+    @SuppressWarnings("unused")
+    static ArgumentSets provisionaCEPOuUFInconsistentes() {
+        return ArgumentSets
+                .argumentsForFirstParameter("XX", "YY", "ZZ", "S1", "12", "S!", "MZ")
+                .argumentsForNextParameter("1234567", "123456789", "12345A78", "12345-678");
+    }
+
+    @CartesianTest
+    @MethodFactory("provisionaDadosNulosOuBrancosParaOEndereco")
     void deveRetornarTrueCasoOEnderecoSejaNuloOuPossuaValoresEmBranco(String logradouro, String numero, String bairro, String cidade, String uf, String cep) {
         EnderecoRequestDTO endereco = new EnderecoRequestDTO(logradouro, numero, bairro, cidade, uf, cep);
         assertTrue(validator.isValid(endereco, contextMock));
+    }
+
+    @SuppressWarnings("unused")
+    static ArgumentSets provisionaDadosNulosOuBrancosParaOEndereco() {
+        return ArgumentSets
+                .argumentsForFirstParameter(null, "", " ", "Av. Bandeirantes")
+                .argumentsForNextParameter(null, "", " ", "21, andar 4, apartamento 42", "43")
+                .argumentsForNextParameter(null, "", " ", "Jardim Planalto")
+                .argumentsForNextParameter(null, "", " ", "São Paulo")
+                .argumentsForNextParameter(null, "", " ", "SP")
+                .argumentsForNextParameter(null, "", " ", "12345678");
     }
 }
--- src/test/java/br/com/matheus/agendamentoconsultas/constraints/validator/ValidTelefoneRequestDTOValidatorTest.java
+++ src/test/java/br/com/matheus/agendamentoconsultas/constraints/validator/ValidTelefoneRequestDTOValidatorTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junitpioneer.jupiter.cartesian.ArgumentSets;
+import org.junitpioneer.jupiter.cartesian.CartesianTest;
+import org.junitpioneer.jupiter.cartesian.CartesianTest.MethodFactory;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -30,36 +33,32 @@ class ValidTelefoneRequestDTOValidatorTest extends MockedUnitTest {
         assertTrue(validator.isValid(telefoneRequestDTO, contextMock), "DDD e número válidos.");
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "'1a', '987654321'",
-            "'11', '98765abcd'",
-            "'11!', '98765@#$%'",
-            "'11', '98765 4321'",
-            "'1 1', '987654321'",
-            "'1', '987654321'",
-            "'111', '987654321'",
-            "'11', '98765432'",
-            "'11', '9876543210'"
-    })
+    @CartesianTest
+    @MethodFactory("provisionaNumerosEDDDsInvalidos")
     void deveRetornarFalseQuandoOFormatoDoTelefoneEstiverInconsistente(String ddd, String numero) {
         TelefoneRequestDTO telefoneRequestDTO = new TelefoneRequestDTO(ddd, numero);
         assertFalse(validator.isValid(telefoneRequestDTO, contextMock));
     }
 
-    @ParameterizedTest
-    @CsvSource(value = {
-            "null, null",
-            "null, '987654321'",
-            "'11', null",
-            "null, null",
-            "'', '987654321'",
-            "'11', ''",
-            "'', ''"
-    }, nullValues = "null")
+    @SuppressWarnings("unused")
+    static ArgumentSets provisionaNumerosEDDDsInvalidos() {
+        return ArgumentSets
+                .argumentsForFirstParameter("1a", "11!", "1 1", "1", "111")
+                .argumentsForNextParameter("98765abcd", "98765@#$%", "9876543210", "98765 4321", "98765432");
+    }
+
+    @CartesianTest
+    @MethodFactory("provisionaNumerosEDDDsNulosBrancosEValidos")
     void deveRetornarTrueCasoOTelefonePossuaValoresEmBranco(String ddd, String numero) {
         TelefoneRequestDTO telefoneRequestDTO = new TelefoneRequestDTO(ddd, numero);
         assertTrue(validator.isValid(telefoneRequestDTO, contextMock));
+    }
+
+    @SuppressWarnings("unused")
+    static ArgumentSets provisionaNumerosEDDDsNulosBrancosEValidos() {
+        return ArgumentSets
+                .argumentsForFirstParameter(null, "", " ", "11")
+                .argumentsForNextParameter(null, "", " ", "987654321");
     }
 
     @ParameterizedTest
