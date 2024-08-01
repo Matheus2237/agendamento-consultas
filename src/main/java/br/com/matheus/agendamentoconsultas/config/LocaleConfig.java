add2741cbad934bdc724a697d6801bbbbcba27b1
Matheus2237 <mathpaulinor@gmail.com>
2024-08-01 17:48:37 -0300

    docs: Documentação da classe LocaleConfig

--- src/main/java/br/com/matheus/agendamentoconsultas/config/LocaleConfig.java
+++ src/main/java/br/com/matheus/agendamentoconsultas/config/LocaleConfig.java
@@ -7,9 +7,30 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
+/**
+ * <p>
+ * Classe de configuração para a resolução de locais
+ * </p>
+ * <p>
+ * Esta classe define um bean do tipo
+ * {@link org.springframework.web.servlet.LocaleResolver} que configura o local padrão
+ * para o Brasil (pt-BR).
+ * </p>
+ *
+ * @author Matheus Paulino Ribeiro
+ * @since 1.0.0
+ */
 @Configuration
 public class LocaleConfig {
 
+    /**
+     * Cria um bean {@link LocaleResolver} configurado para o local padrão pt-BR
+     * <p>
+     * Este método define um bean Spring do tipo LocaleResolver. O resolvedor de local
+     * é configurado para usar o local padrão do Brasil (pt-BR).
+     *
+     * @return Um bean {@link LocaleResolver} configurado para pt-BR
+     */
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
