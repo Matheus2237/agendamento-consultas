ea7637d10b021e7f7fe3c02b5388fe75d10f1b1f
Matheus2237 <mathpaulinor@gmail.com>
2024-08-01 17:46:46 -0300

    test: Teste do LocaleResolver

---
+++ src/test/java/br/com/matheus/agendamentoconsultas/config/LocaleConfigTest.java
@@ -0,0 +1,23 @@
+package br.com.matheus.agendamentoconsultas.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocaleConfigTest {
+
+    @Test
+    void deveConfigurarUmResolverDeLocaleParaPtBR() {
+        LocaleConfig localeConfig = new LocaleConfig();
+        LocaleResolver localeResolver = localeConfig.localeResolver();
+        assertInstanceOf(SessionLocaleResolver.class, localeResolver, "O resolver deve ser uma instância de SessionLocaleResolver.");
+        SessionLocaleResolver resolver = (SessionLocaleResolver) localeResolver;
+        Locale locale = ReflectionTestUtils.invokeMethod(resolver, "getDefaultLocale");
+        assertEquals(Locale.of("pt", "BR"), locale, "O local padrão deve ser pt-BR.");
+    }
+}
