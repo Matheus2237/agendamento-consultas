9418e97eb97acf587469748480acc38445384ee0
Matheus2237 <mathpaulinor@gmail.com>
2025-03-28 13:55:53 -0300

    refactor: Remoção de métodos sobrescritos não utilizados das entidades

--- src/main/java/br/com/matheus/agendamentoconsultas/model/vo/CRM.java
+++ src/main/java/br/com/matheus/agendamentoconsultas/model/vo/CRM.java
@@ -31,22 +31,4 @@ public class CRM {
     @NotBlank
     @Column(name = "crm", unique = true, nullable = false)
     private String value;
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        CRM crm = (CRM) obj;
-        return Objects.equals(value, crm.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
-        return this.value;
-    }
 }
