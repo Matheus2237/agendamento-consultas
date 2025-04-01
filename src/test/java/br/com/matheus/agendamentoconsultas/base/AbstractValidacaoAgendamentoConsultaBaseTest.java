211c6ae41243082738c34389071aab837a97f4a3
Matheus2237 <mathpaulinor@gmail.com>
2025-03-31 21:49:21 -0300

    refactor: Utilização do método utilitário para setar o id das entidades

--- src/test/java/br/com/matheus/agendamentoconsultas/base/AbstractValidacaoAgendamentoConsultaBaseTest.java
+++ src/test/java/br/com/matheus/agendamentoconsultas/base/AbstractValidacaoAgendamentoConsultaBaseTest.java
@@ -4,14 +4,13 @@ import br.com.matheus.agendamentoconsultas.model.Consulta;
 import br.com.matheus.agendamentoconsultas.model.Medico;
 import br.com.matheus.agendamentoconsultas.model.Paciente;
 import br.com.matheus.agendamentoconsultas.model.enums.DiaDaSemana;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeAll;
 
-import java.lang.reflect.Field;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 import static br.com.matheus.agendamentoconsultas.model.enums.DiaDaSemana.getDiaDaSemanaPelaData;
+import static br.com.matheus.agendamentoconsultas.util.ReflectionUtil.setPrivateId;
 
 /**
  * Classe base para testes de validação de agendamento de consultas, configurando entidades e dados necessários.
@@ -34,20 +33,13 @@ public abstract class AbstractValidacaoAgendamentoConsultaBaseTest extends Mocke
     static void setUpBeforeAll() {
         medicoId = 1L;
         medico = new Medico();
-        setPrivateField(medico, medicoId);
+        setPrivateId(medico, medicoId);
         pacienteId = 1L;
         paciente = new Paciente();
-        setPrivateField(paciente, pacienteId);
+        setPrivateId(paciente, pacienteId);
         data = LocalDate.now().plusDays(1);
         horarioConsulta = LocalTime.parse("10:00");
         diaDaSemana = getDiaDaSemanaPelaData(data);
-    }
-
-    @SneakyThrows
-    public static void setPrivateField(Object entity, Long id) {
-        Field field = entity.getClass().getDeclaredField("id");
-        field.setAccessible(true);
-        field.set(entity, id);
     }
 
     /**
